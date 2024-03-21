e7eaa79e0c98ca5f78355837e3b2f8333899da3d
jmpark <stylack@gamil.com>
2024-03-21 17:54:21 +0900

    About, Support 컴포넌트 생성

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section name="about" className="w-full h-screen">
+      <div className="flex flex-col justify-center w-full h-full max-w-screen-lg p-4 mx-auto">
+        <div className="text-center">
+          <h2 className="text-5xl font-bold">About this company</h2>
+          <p className="py-6 text-3xl text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <p className="mt-20 text-xl">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium consectetur in alias magnam, officia
+          rerum quam sunt exercitationem impedit porro? Autem unde culpa quae rem aliquam doloribus sunt! Quod ad libero
+          hic quos laborum repellat dolore voluptates iste esse unde, deleniti fugit porro similique necessitatibus quo?
+          Qui molestiae voluptatem labore.
+        </p>
+        <p className="mt-20 text-xl">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium consectetur in alias magnam, officia
+          rerum quam sunt exercitationem impedit porro? Autem unde culpa quae rem aliquam doloribus sunt! Quod ad libero
+          hic quos laborum repellat dolore voluptates iste esse unde, deleniti fugit porro similique necessitatibus quo?
+          Qui molestiae voluptatem labore.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/components/Support.js
+++ src/components/Support.js
@@ -1,7 +1,69 @@
 import React from 'react';
+import supportImg from '../assets/support.jpeg';
+import { FaArrowRight } from 'react-icons/fa';
 
 const Support = () => {
-  return <div>Support</div>;
+  return (
+    <section name="support" className="w-full mt-24">
+      <div className="w-full h-[700px] bg-slate-900 absolute">
+        <img src={supportImg} alt="support" className="object-cover w-full h-full mix-blend-overlay" />
+      </div>
+      <div className="max-w-[1240px] mx-suto text-white relative">
+        <div className="px-4 py-12">
+          <h2 className="pt-8 text-3xl text-center uppercase text-slate-300">Support</h2>
+          <h3 className="py-6 text-5xl font-bold text-center">We support you</h3>
+          <p className="py-4 text-3xl text-slate-300">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia omnis magni quos voluptatibus, dignissimos
+            quisquam?
+          </p>
+        </div>
+        <div className="grid grid-cols-1 lg:grid-cols-3 px-4 pt-12 text-black gap-x-8 gap-y-16 sm:pt-20">
+          <div className="bg-white shadow-2xl rounded-xl">
+            <div className="p-8">
+              <h3 className="my-6 text-2xl font-bold">Support</h3>
+              <p className="text-xl text-gray-600">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, quod doloremque dolorem unde odit
+                corrupti!
+              </p>
+            </div>
+            <div className="py-4 pl-8 bg-slate-100">
+              <p className="flex items-center text-blue-600 cursor-pointer">
+                More Info... <FaArrowRight className="w-5 ml-2" />
+              </p>
+            </div>
+          </div>
+          <div className="bg-white shadow-2xl rounded-xl">
+            <div className="p-8">
+              <h3 className="my-6 text-2xl font-bold">Support</h3>
+              <p className="text-xl text-gray-600">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, quod doloremque dolorem unde odit
+                corrupti!
+              </p>
+            </div>
+            <div className="py-4 pl-8 bg-slate-100">
+              <p className="flex items-center text-blue-600 cursor-pointer">
+                More Info... <FaArrowRight className="w-5 ml-2" />
+              </p>
+            </div>
+          </div>
+          <div className="bg-white shadow-2xl rounded-xl">
+            <div className="p-8">
+              <h3 className="my-6 text-2xl font-bold">Support</h3>
+              <p className="text-xl text-gray-600">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, quod doloremque dolorem unde odit
+                corrupti!
+              </p>
+            </div>
+            <div className="py-4 pl-8 bg-slate-100">
+              <p className="flex items-center text-blue-600 cursor-pointer">
+                More Info... <FaArrowRight className="w-5 ml-2" />
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Support;
