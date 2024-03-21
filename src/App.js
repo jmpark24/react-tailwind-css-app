8b8243fe23b72e455269705844770a12bd6a3a00
jmpark <stylack@gamil.com>
2024-03-21 16:38:06 +0900

    tailwindcss 적용 완료

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Feature from './components/Feature';
+import Feedback from './components/Feedback';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Support from './components/Support';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Home />
+      <About />
+      <Support />
+      <Feature />
+      <Feedback />
+      <Contact />
+      <Footer />
+    </>
   );
 }
 
