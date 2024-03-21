8c2344a11b9ae1ccb82c3aef6c49e4b2988652bf
jmpark <stylack@gamil.com>
2024-03-21 18:42:31 +0900

    Contact, Footer 컴포넌트 생성

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,44 @@
 import React from 'react';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section name="contact" className="w-full h-screen p-4">
+      <div className="flex flex-col justify-center h-full max-w-screen-lg p-4 mx-auto">
+        <div className="text-center">
+          <h2 className="text-5xl font-bold">Contact</h2>
+          <p className="py-6 text-3xl text-gray-500">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta perspiciatis veritatis, ullam ab voluptate
+            voluptatem.
+          </p>
+        </div>
+        <div className="flex items-center justify-center mt-10">
+          <form className="flex flex-col w-full md:w-1/2">
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter your name"
+              className="p-2 border-2 rounded-md focus:outline-none"
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter your email"
+              className="p-2 border-2 mt-4 rounded-md focus:outline-none"
+            />
+            <textarea
+              name="message"
+              placeholder="Enter your message"
+              className="p-2 border-2 rounded-md focus:outline-none mt-4"
+              rows="10"
+            ></textarea>
+            <button className="duration-300 bg-gradient-to-b from-cyan-500 to-blue-500 hover:scale-110 my-8 mx-auto px-6 py-3">
+              Let's Talk
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,68 @@
 import React from 'react';
+import { FaFacebook, FaGithub, FaInstagram, FaTwitch, FaTwitter } from 'react-icons/fa';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <section className="w-full px-2 text-gray-300 bg-slate-700">
+      <div className="max-w-[1240px] mx-auto grid grid-cols-2 md:grid-cols-6 border-b-2 border-gray-600 py-8">
+        <div className="text-center">
+          <h6 className="pt-2 font-bold uppercase">Solutions</h6>
+          <ul>
+            <li className="py-1">Marketing</li>
+            <li className="py-1">Analytics</li>
+            <li className="py-1">Commerce</li>
+            <li className="py-1">Data</li>
+          </ul>
+        </div>
+        <div className="text-center">
+          <h6 className="pt-2 font-bold uppercase">Solutions</h6>
+          <ul>
+            <li className="py-1">Marketing</li>
+            <li className="py-1">Analytics</li>
+            <li className="py-1">Commerce</li>
+            <li className="py-1">Data</li>
+          </ul>
+        </div>
+        <div className="text-center">
+          <h6 className="pt-2 font-bold uppercase">Solutions</h6>
+          <ul>
+            <li className="py-1">Marketing</li>
+            <li className="py-1">Analytics</li>
+            <li className="py-1">Commerce</li>
+            <li className="py-1">Data</li>
+          </ul>
+        </div>
+        <div className="text-center">
+          <h6 className="pt-2 font-bold uppercase">Solutions</h6>
+          <ul>
+            <li className="py-1">Marketing</li>
+            <li className="py-1">Analytics</li>
+            <li className="py-1">Commerce</li>
+            <li className="py-1">Data</li>
+          </ul>
+        </div>
+        <div className="col-span-2 pt-8 md:pt-2">
+          <p className="font-bold uppercase">Subscribe to our newsletter</p>
+          <p className="py-4">The latest news, articles, and resource, sent to your inbox weekly.</p>
+          <form className="flex flex-col sm:flex-row">
+            <input className="w-full p-2 mb-4 mr-4 rounded-md" type="email" placeholder="Enter email..." />
+            <button className="p-2 mb-4">Subscribe</button>
+          </form>
+        </div>
+      </div>
+
+      <div className="flex flex-col max-w-[1240px] px-2 py-4 mx-auto justify-between sm:flex-row text-center text-gray-500">
+        <p className="py-4">2022 Chatty, mLLc. All rights reserved.</p>
+        <div className="flex justify-between sm:w-[300px] pt-4 text-2xl">
+          <FaFacebook />
+          <FaInstagram />
+          <FaTwitter />
+          <FaTwitch />
+          <FaGithub />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Footer;
