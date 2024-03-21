0b5127faedde771d815bb92213656604b4f3df81
jmpark <stylack@gamil.com>
2024-03-21 17:34:27 +0900

    Navbar, Home 컴포넌트 생성

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,33 @@
 import React from 'react';
-
+import apple from '../assets/apple.svg';
+import google from '../assets/google.svg';
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section name="home" className="flex w-full h-screen bg-zinc-200">
+      <div className="grid md:grid-cols-2 max-w-[1240px] m-auto">
+        <div className="flex flex-col justify-center w-full px-2 py-8 md:items-start">
+          <p className="text-2xl">Use our chat platform</p>
+          <h1 className="py-3 text-5xl font-bold md:text-7xl">Chat management</h1>
+          <p className="text-2xl">Chatty is the best chat platform.</p>
+          <button className="py-3 px-6 sm:w-[60%] my-4">Get Started</button>
+        </div>
+        <div className="flex flex-col justify-center px-2">
+          <h2 className="font-semibold xs:text-[48px] text-[40px] xs:leading-[76.8px] leading-[66.8px] w-full">
+            Easity download app <br className="gidden sm:block" /> Google & Apple
+          </h2>
+          <p className="text-[18px] leading-[30.8px] max-w-[470px] mt-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores exercitationem molestiae sit, incidunt
+            neque adipisci porro minima ullam necessitatibus similique distinctio! Soluta nesciunt nobis nisi fugiat
+            molestias mollitia rerum facere?
+          </p>
+          <div className="flex flex-row flex-wrap mt-6 sm:mt-10">
+            <img src={apple} alt="apple_pay" className="w-[128.86px] h-[42.05px] object-contain mr-5 cursor-pointer" />
+            <img src={google} alt="google_pay" className="w-[144.17px] h-[43.08px] object-contain cursor-pointer" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaAlignJustify, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [nav, setNav] = useState(false);
+  return (
+    <nav className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
+      <div className="flex items-center justify-between w-full h-full px-10">
+        <div className="flex items-center">
+          <h1 className="mr-4 text-3xl font-bold sm:text-4xl">Chatty</h1>
+          <ul className="hidden md:flex">
+            <li>Home</li>
+            <li>About</li>
+            <li>Support</li>
+            <li>Feature</li>
+            <li>Feedback</li>
+          </ul>
+        </div>
+        <div className="hidden pr-4 md:flex">
+          <button className="text-slate-600 mr-4 bg-transparent border-none">Sign In</button>
+          <button className="px-8 py-3">Sign up</button>
+        </div>
+        <div
+          onClick={() => {
+            setNav(!nav);
+          }}
+          className="mr-4 md:hidden hover:cursor-pointer"
+        >
+          {nav ? <FaTimes /> : <FaAlignJustify />}
+        </div>
+      </div>
+      <ul className={nav ? 'absolute bg-zinc-200 w-full px-8' : 'hidden'}>
+        <li className="w-full border-b-2 border-zinc-300">Home</li>
+        <li className="w-full border-b-2 border-zinc-300">About</li>
+        <li className="w-full border-b-2 border-zinc-300">Support</li>
+        <li className="w-full border-b-2 border-zinc-300">Feature</li>
+        <li className="w-full border-b-2 border-zinc-300">Feedback</li>
+        <div className="flex flex-col my-4">
+          <button className="px-8 py-3 mb-4 text-slate-600 bg-transparent">Sign In</button>
+          <button className="px-8 py-3">Sign Up</button>
+        </div>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
